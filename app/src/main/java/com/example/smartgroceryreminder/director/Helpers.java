4ac0c088950166290a27f1db0e8a2cd34c6856a0
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2020-06-08 13:07:32 +0500

    Working on Alarm

--- app/src/main/java/com/example/smartgroceryreminder/director/Helpers.java
+++ app/src/main/java/com/example/smartgroceryreminder/director/Helpers.java
@@ -207,13 +207,13 @@ public class Helpers {
 
         Calendar cal = Calendar.getInstance();
         // add 30 seconds to the calendar object
-        cal.add(Calendar.SECOND, 15);
-//        cal.set(Calendar.YEAR, year);
-//        cal.set(Calendar.MONTH, month - 1);
-//        cal.set(Calendar.DAY_OF_MONTH, day);
-//        cal.set(Calendar.HOUR_OF_DAY, hour);
-//        cal.set(Calendar.MINUTE, minute);
-//        cal.set(Calendar.SECOND, 0);
+//        cal.add(Calendar.SECOND, 15);
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month - 1);
+        cal.set(Calendar.DAY_OF_MONTH, day);
+        cal.set(Calendar.HOUR_OF_DAY, hour);
+        cal.set(Calendar.MINUTE, minute);
+        cal.set(Calendar.SECOND, 0);
 
         Intent intent = new Intent(context, AlarmReceiver.class);
         intent.setFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
